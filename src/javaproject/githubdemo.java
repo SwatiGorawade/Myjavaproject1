9cfc2189496bff4f8df4d4bfdc19399853373056
SwatiGorawade <gorawadeswati@gmail.com>
2022-07-18 15:38:04 +0530

    First commit

---
+++ src/javaproject/githubdemo.java
@@ -0,0 +1,12 @@
+package javaproject;
+
+public class githubdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "message from lead";
+		System.out.println(str);
+
+	}
+
+}
